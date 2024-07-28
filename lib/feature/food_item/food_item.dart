04fb35f439759ccd8c22a91c9f35eb6ba8aad2f3
Azorli <arthurgagliardiazorli@gmail.com>
2024-07-27 21:15:04 -0300

    change design

--- lib/feature/food_item/food_item.dart
+++ lib/feature/food_item/food_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../core/domain/food.dart';
-import '../core/widgets/galaxy_button.dart';
+import '../../core/widgets/galaxy_button.dart';
 
 enum FoodItemSize{
   big,
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:galaxy_food/feature/home_page.dart';
+import 'package:galaxy_food/feature/home_page/home_page.dart';
+import 'package:galaxy_food/feature/order_page/order_page.dart';
 import 'package:galaxy_food/galaxy_theme.dart';
 
 main(){
@@ -18,11 +20,30 @@ class AppState extends State<App>{
 
   @override
   Widget build(BuildContext context) {
+    final theme = GalaxyFoodTheme.normal;
+
     return MaterialApp(
       themeMode: ThemeMode.dark,
       theme: GalaxyFoodTheme.normal,
       home: Scaffold(
-        body: HomePage(),
+        bottomNavigationBar: Container(
+          height: 78,
+          color: theme.colorScheme.secondary,
+          child: CurvedNavigationBar(
+            animationCurve: Curves.fastEaseInToSlowEaseOut,
+            backgroundColor: Colors.transparent,
+            buttonBackgroundColor: theme.colorScheme.primary,
+            color: theme.colorScheme.primaryContainer,
+            animationDuration: kThemeAnimationDuration,
+            items: const [
+              Icon(Icons.home, size: 30),
+              Icon(Icons.shopping_cart_outlined, size: 30),
+              Icon(Icons.history, size: 30),
+              Icon(Icons.settings, size: 30),
+            ],
+          ),
+        ),
+        body: const OrderPage(),
       ),
     );
   }
