d88af76acc16f4f5f5809d0956fa1a3320e3ee4a
Azorli <arthurgagliardiazorli@gmail.com>
2024-08-12 22:01:50 -0300

    change app theme

--- lib/galaxy_theme.dart
+++ lib/galaxy_theme.dart
@@ -18,9 +18,11 @@ class GalaxyFoodTheme {
           primaryContainer: Color(0xff000000),
           onPrimaryContainer: Color(0xca000000),
           secondaryContainer: Color(0x67040404),
+          tertiaryContainer: Color(0xff2c2c2c),
           inverseSurface: Color(0xffafafaf),
+          inversePrimary: Colors.orangeAccent,
           error: Color(0xffbb0000),
-          onError: Color(0xffa90000)
+          onError: Color(0xffff2121)
       ),
       fontFamily: 'Montserrat',
       textTheme: const TextTheme(
@@ -28,19 +30,19 @@ class GalaxyFoodTheme {
             fontFamily: 'Montserrat',
             color: Color(0xff000000),
             fontSize: 57,
-            fontWeight: FontWeight.w600,
+          fontWeight: FontWeight.w600,
         ),
         displayMedium: TextStyle(
             fontFamily: 'Montserrat',
             color: Color(0xff000000),
             fontSize: 45,
-            fontWeight: FontWeight.w600,
+          fontWeight: FontWeight.w600,
         ),
         displaySmall: TextStyle(
-            fontFamily: 'Montserrat',
-            color: Color(0xffffffff),
-            fontSize: 36,
-            fontWeight: FontWeight.w600,
+          fontFamily: 'Montserrat',
+          color: Color(0xffffffff),
+          fontSize: 36,
+          fontWeight: FontWeight.w600,
         ),
         headlineLarge: TextStyle(
           fontFamily: 'Montserrat',
