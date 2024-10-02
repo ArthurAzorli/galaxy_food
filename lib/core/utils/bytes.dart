7770a5b94b78b6634bc7442da16b58c48998f5c9
Azorli <arthurgagliardiazorli@gmail.com>
2024-10-01 22:32:56 -0300

    add Uint8List parser

--- lib/core/utils/bytes.dart
+++ lib/core/utils/bytes.dart
@@ -1,8 +1,13 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-extension BytesUtils on Uint8List{
+extension BytesUtilsUintList on Uint8List{
 
   String get toUTF8 => utf8.decode(this);
 
+}
+
+extension BytesUtilsListInt on List<int>{
+
+  Uint8List get toUint8List => Uint8List.fromList(this);
 }
